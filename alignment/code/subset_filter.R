0aae525f654e266b868bf9ea79cce18b65b1f6cb
Jaehee Kim <jaehee2236@gmail.com>
2020-05-26 18:28:21 -0700

    subset and filter script

---
+++ alignment/code/subset_filter.R
@@ -0,0 +1,128 @@
+## This script is to subset sequences for specific countries (or regions)
+#  and filter sites for missing data within the subsetted sequeces.
+
+rm(list=ls())
+
+library(Rsamtools)
+library(Biostrings)
+
+# If you want to add reference genome to the output file, 
+# do the following in the "Subset for country" part
+# (you can order of the reference genome the way you want):
+# headers <- headers.all[c(country.ind, ref.ind)]
+
+# ================================
+data.dir <- '~/Documents/Covid_Analysis/'
+setwd(data.dir)
+
+fasta.f <- 'all_seq.fasta'
+# fasta.f <- 'all_seq_reflen.fasta'
+meta.f <- 'all_meta.tsv'
+
+country <- 'USA'
+
+cat(paste('\n================ Processing', country, '================\n'))
+
+# ================================
+meta <- read.delim(meta.f, header=TRUE, as.is=TRUE)
+
+if (!file.exists(sprintf("%s.fai", fasta.f))) {
+    cat('\n Indexing all sequences. \n')
+    indexFa(fasta.f)
+    cat('\n Indexing all sequences completed. \n')
+}
+
+fa <- open(FaFile(fasta.f))
+idx <- scanFaIndex(fa)
+n.seq <- countFa(fa)
+headers.all <- as.character(seqnames(idx))
+
+ref.id <- 'hCoV-19/Wuhan-Hu-1/2019|EPI_ISL_402125|2019-12-31'
+ref.ind <- which(headers.all == ref.id)
+ref.seq <- scanFa(fa, param=idx[ref.ind])[1][[1]]
+
+tot.seq.len <- length(ref.seq)
+stopifnot(all(width(ranges(idx)) == tot.seq.len)) # make sure all sequences are the same length
+cat(paste('\nTotal number of sequences:', n.seq, '\n'))
+cat(paste('Total length of per sequence:', tot.seq.len, '\n\n'))
+
+ref.seq.len <- 29903 # what it should be based on genbank MN908947
+if (tot.seq.len == ref.seq.len) {
+    cat('All sequences have the same length as the MN908947 reference sequence.\n')
+    gap.pos <- unlist(gregexpr(pattern='-', ref.seq))
+    stopifnot(gap.pos == -1)
+}
+
+## =======================================================
+# Subset for country
+country.ind <- which(meta$country == country)
+cat(paste('\nTotal number of all sequences from', country, ':', 
+          length(country.ind), '\n'))
+
+headers <- headers.all[country.ind]
+# headers <- headers.all[c(country.ind, ref.ind)] #uncomment to include ref seq
+country.seq <- scanFa(fa, param=GRanges(seqnames=headers, 
+                                        IRanges(start=1, width=tot.seq.len)))
+
+## =======================================================
+# Filtering 1
+# Remove the samples that have more than 10% of 
+# its sites missing or ambiguous.
+
+seq.th <- 0.1 # missing fraction threshold
+site.stat <- alphabetFrequency(country.seq, as.prob=TRUE)
+to.include.seq <- which(1 - rowSums(site.stat[ ,1:4]) <= seq.th)
+
+cat(paste('\n# of sequences from', country, 
+          'with more than', seq.th, 'of sites missing or ambiguous are', 
+          length(country.seq) - length(to.include.seq), 'of',  
+          length(country.seq), '\n'))
+
+country.seq <- country.seq[to.include.seq]
+headers <- headers[to.include.seq]
+
+## =======================================================
+# Filtering 2
+# Remove the sites that have missing or ambiguous values 
+# in more than 20% of the samples.
+
+site.th <- 0.2 # missing fraction threshold
+sum.mat <- consensusMatrix(country.seq, as.prob=TRUE) 
+to.include.site <- which(1 - colSums(sum.mat[1:4, ]) <= site.th)
+to.exclude <- which(1 - colSums(sum.mat[1:4, ]) > site.th)
+
+cat(paste('# of sites with more than', site.th, 
+          'of sequences missing or ambiguous are', 
+          tot.seq.len - length(to.include.site), 'of',  
+          tot.seq.len, '\n\n'))
+
+## =======================================================
+# Additional filtering and site subsetting goes here.
+
+
+write.range <- reduce(IRanges(start=to.include.site, end=to.include.site))
+
+# =====================================
+# ==== subset sites and write file ====
+# =====================================
+stopifnot(length(country.seq) == length(headers))
+save.f <- paste(country, '.fasta', sep='')
+
+country.out <- list()
+for (i in 1:length(write.range)) {
+    s.name <- headers[i]
+    tmp <- subseq(country.seq, write.range[i])
+    country.out[[i]] <- tmp
+}
+
+cat('Subsetting done. \n')
+cat('Gathering sequences. \n')
+country.out <- DNAStringSet(do.call(paste0, lapply(country.out, as.character)))
+names(country.out) <- headers
+cat('Writing file. \n')
+writeXStringSet(country.out, file=save.f, append=FALSE)
+
+cat(paste('\nDone! \nOutput is written to', data.dir, save.f, '\n', sep=''))
+
+close(fa)
+
